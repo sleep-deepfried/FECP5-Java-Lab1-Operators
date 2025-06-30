a80494f2824f8e753cc415e1afd8647a1be92afb
Earl John Pulido <earljohn.pulido@gfitftlapt02381.ad.mynt.xyz>
2025-06-30 14:49:14 +0800

    Final Submission, Lab 1: Celsius to Fahrenheit Converter

---
+++ README.md
@@ -0,0 +1,8 @@
+## Lab 1: Celsius to Fahrenheit Converter
+Write a program that takes in a float Celsius, and displays its equivalent value in Fahrenheit in two decimal places. The formula is as follows: °F = °C × (9/5) + 32.
+
+
+## Screenshot of Output
+![img.png](img.png)
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
-
+import java.util.Scanner;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Temperature in Celsius: ");
+        float celsius = scanner.nextInt();
+        float fahrenheit = (celsius * (9.0f/5.0f) + 32.0f);
+        System.out.println("Temperature in Fahrenheit: ".concat(String.format("%.2f",fahrenheit)));
 
     }
 }
